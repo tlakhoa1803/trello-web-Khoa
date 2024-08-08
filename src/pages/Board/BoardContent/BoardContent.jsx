fca650ac84d6f9acf00eacdd46baacf176b01bf9
TobiraKhoa Senju <tlakhoa1803@gmail.com>
2024-08-08 14:13:10 +0700

    Finish Drag

--- src/pages/Board/BoardContent/BoardContent.jsx
+++ src/pages/Board/BoardContent/BoardContent.jsx
@@ -8,14 +8,15 @@ import {
   TouchSensor,
   useSensors,
   DragOverlay,
-  defaultDropAnimationSideEffects
+  defaultDropAnimationSideEffects,
+  closestCorners
   // PointerSensor
 } from '@dnd-kit/core'
 import { useEffect, useState } from 'react'
 import { arrayMove } from '@dnd-kit/sortable'
 import Cards from './ListColumns/Columns/ListCards/Cards/Cards'
 import Columns from './ListColumns/Columns/Columns'
-import { colors } from '@mui/material'
+import { cloneDeep } from 'lodash'
 
 function BoardContent({ board }) {
   const ACTIVE_DRAG_ITEMS_TYPE = {
@@ -24,10 +25,13 @@ function BoardContent({ board }) {
   }
   //Yêu cầu chuột di chuyển 10px thì kích hoạt event
   // const pointerSensor = useSensor(PointerSensor, { activationConstraint: { distance: 10 } })
+  // Chuột di chuyển 10px mới kích hoạt event
   const mouseSensor = useSensor(MouseSensor, { activationConstraint: { distance: 10 } })
+  // Dùng chạm dùng cho điện thoại
   const touchSensor = useSensor(TouchSensor, { activationConstraint: { delay: 250, tolerance: 500 } })
   const sensors = useSensors(mouseSensor, touchSensor )
   const [orderedColumns, setOrderedColumns] = useState([])
+  //Cùng 1 thời điểm chỉ có thể kéo 1 phần tử là column hoặc card
   const [activeDragItemId, setActiveDragItemId] = useState(null)
   const [activeDragItemType, setActiveDragItemType] = useState(null)
   const [activeDragItemData, setActiveDragItemData] = useState(null)
@@ -35,10 +39,11 @@ function BoardContent({ board }) {
   useEffect(() => {
     setOrderedColumns(mapOrder(board?.columns, board?.columnOrderIds, '_id'))
   }, [board] )
-
-  const findColumnCardId = (cardID) => {
-    return orderedColumns.find(column => column.cards.map( card => card._id ) ?. includes(cardID))
+  // Tìm 1 column theo Card ID
+  const findColumnCardId = (cardId) => {
+    return orderedColumns.find(column => column?.cards?.map( card => card._id )?.includes(cardId))
   }
+  //Lưu trữ dữ liệu của phần tử đang được kéo
   const handleDragStart =(event) => {
     setActiveDragItemId( event?.active?.id )
     setActiveDragItemType(event?.active?.data?.current?.columnId ? ACTIVE_DRAG_ITEMS_TYPE.CARD : ACTIVE_DRAG_ITEMS_TYPE.COLUMN)
@@ -48,20 +53,52 @@ function BoardContent({ board }) {
   // Trigger trong quá trình kéo (Drag) 1 phần tử
   const handleDragOver = (event) => {
     // Không làm gì thêm nếu kéo thả Columns
-    if ( activeDragItemType === ACTIVE_DRAG_ITEMS_TYPE.CARD ) {
+    if ( activeDragItemType === ACTIVE_DRAG_ITEMS_TYPE.COLUMN ) {
       return
     }
     //Xử lí kéo (Drag) Cards
-    console.log('Handle Drag Over', event)
     const { active, over } = event
     // Nếu kéo ra khỏi mà không có ô chưa thì return
     if (! active || !over) return
     //activeDragingCardId là card đang được kéo
-    const { id: activeDragingCardId, data: { current: activeDragingCardData } } = active
+    const { id: activeDragingCardId, data: { current: activeDraggingCardData } } = active
     //overCardId là card tương tác với card được kéo
     const { id: overCardId } = over
     // Tìm 2 column theo CardId
+    const activeColumn = findColumnCardId( activeDragingCardId )
+    const overColumn = findColumnCardId (overCardId)
+    // Nếu ko tồn tại 1 trong 2 column thì không làm gì hết
+    if (!activeColumn || !overColumn) return
+    // Xử lý logic ở đây chỉ khi kéo card qua 2 column khác nhau, còn nếu kéo card trong chính column ban đầu của nó thì không làm gì
+    // Vì đây đang là đoạn xử lý lúc kéo (handleDragOver), còn xử lý lúc kéo xong xuôi thì nó lại là vấn đề khác ở (handleDragEnd)
+    if (activeColumn._id !== overColumn._id) {
+      setOrderedColumns (prevColumns => {
+      // Tìm vị trí index của overCard trong Column mà card được thả
+        const overCardIndex = overColumn?.cards?.findIndex(card => card._id === overCardId)
+        let newCardIndex
+        const isBelowOverItem =active.rect.current.translated && active.rect.current.translated.top > over.rect.top + over.rect.height
+        const modifier = isBelowOverItem ? 1 : 0
+        newCardIndex = overCardIndex >= 0 ? overCardIndex + modifier : overColumn?.cards?.length + 1
 
+        const nextColumns = cloneDeep(prevColumns)
+        const nextActiveColumn = nextColumns.find(column => column._id === activeColumn._id)
+        const nextOverColumn = nextColumns.find(column => column._id === overColumn._id)
+        //Column cũ
+        if (nextActiveColumn) {
+          //Xóa card ở Column cũ
+          nextActiveColumn.cards= nextActiveColumn.cards.filter(card => card._id !== activeDragingCardId)
+          //Cập nhật lại mảng cardOrderIds cho chuẩn dữ liệu
+          nextActiveColumn.cardOrderIds = nextActiveColumn.cards.map( card => card._id )
+        }
+        //Column mới
+        if (nextOverColumn) {
+          // Kiểm tra card đang kéo có tồn tại ở Over Column chưa, nếu có thì xóa thằng ở vị trí cũ
+          nextOverColumn.cards = nextOverColumn.cards.filter(card => card._id !== activeDragingCardId)
+          //Thêm card đang kéo vào OverColumn vị trí mới
+          nextOverColumn.cards = nextOverColumn.cards.toSpliced(newCardIndex, 0, activeDraggingCardData)
+        }
+        return nextColumns
+      }) }
   }
   const handleDragEnd = (event) => {
     if ( activeDragItemType === ACTIVE_DRAG_ITEMS_TYPE.CARD ) {
@@ -87,7 +124,8 @@ function BoardContent({ board }) {
   const dropAnimation = {
     sideEffects: defaultDropAnimationSideEffects({ styles: { active: { opacity: '0.5' } } } ) }
   return (
-    <DndContext onDragEnd={ handleDragEnd } onDragOver={ handleDragOver } onDragStart={ handleDragStart } sensors={sensors}>
+    <DndContext onDragStart={ handleDragStart } onDragOver={ handleDragOver } onDragEnd={ handleDragEnd } sensors={sensors}
+      collisionDetection={ closestCorners }>
       <Box sx={{
         bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#34495e': '#3498db'),
         width:'100%',
